a0c240e12852115a1dc7782dc8b3185326c3e73a
kevinchesser <kevin.chesser94@gmail.com>
2017-05-09 23:11:55 -0400

    add init function, check if message from correct channel

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
-var Discordordie = require("discordie");
+var Discordie = require("discordie");
 var client = new Discordie();
+
+var testChannel;
+var state; //0 idle, 1 active
 
 client.connect({
     token: ""
 });
 
 client.Dispatcher.on("GATEWAY_READY", e => {
+        initBot();
+        testChannel.sendMessage("The Kruelmonster is approaching!");
         console.log("Connected as: " + client.User.username);
 });
 
 client.Dispatcher.on("MESSAGE_CREATE", e => {
-    if(e.message.content == "ping")
-        e.message.channel.sendMessage("pong");
+    if(e.message.channel.id == ""){
+        if(e.message.content == "km attacks"){
+            state = 1; //set bot to active state
+            testChannel.sendMessage("pong");
+        }
+        console.log(e.message);
+    }
 });
+
+function initBot(){
+    testChannel = client.Channels.get("");
+    state = 0; //set bot to idle state
+}
